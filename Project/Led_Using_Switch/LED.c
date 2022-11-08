eb60d148907bfcbaa8ebba059d5642d0e3770660
Balkumar Vishwakarma <105841489+embeddedsoftware8090@users.noreply.github.com>
2022-11-08 14:18:21 +0530

    Add files via upload

---
+++ Project/Led_Using_Switch/LED.c
@@ -0,0 +1,16 @@
+#include<reg51.h>
+#include<dela.h>
+sbit LED =P1^0;
+sbit SW  =P1^0;
+main()
+{
+    while(1);
+    {
+        if (SW == 0)//Pressed
+            LED = 0;//LED ON
+        else
+            LED =1;//LED OFF
+        
+    }
+ 
+}
